394195aca06b3d1a2fc0cf14949170d1bf16b313
Melissa Zhang <mfz2@duke.edu>
2016-03-07 10:05:47 -0500

    viewpalettes class

--- src/view/ViewPalettes.java
+++ src/view/ViewPalettes.java
@@ -1,19 +1,40 @@
 package view;
 
+import java.util.ResourceBundle;
+
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class ViewPalettes extends View{
-
+	private static final String PALETTE_PROPERTIES = "Palettes";
+	private static final int PALETTE_SIZE = 15;
+	private ResourceBundle myResources;
+	private Pane viewPane;
+	private VBox vbox;
 
 	public ViewPalettes(ViewType ID) {
 		super(ID);
-		// TODO Auto-generated constructor stub
+		viewPane = new Pane();
+		myResources = ResourceBundle.getBundle(PALETTE_PROPERTIES);
+		viewPane.getChildren().add(vbox);
 	}
 
 	@Override
 	public Pane getView() {
-		// TODO Auto-generated method stub
-		return null;
+		setUpColorPalette();
+		setUpShapePalette();
+		
+		return viewPane;
+	}
+
+	private void setUpShapePalette() {
+		ShapePalette shapePalette = new ShapePalette(myResources.getString("SHAPES"),PALETTE_SIZE);
+		vbox.getChildren().add(shapePalette.getPaletteViewGroup());
+	}
+
+	private void setUpColorPalette() {
+		CustomColorPalette colorPalette = new CustomColorPalette(myResources.getString("CUSTOMCOLORS"),PALETTE_SIZE);
+		vbox.getChildren().add(colorPalette.getPaletteViewGroup());
 	}
 
 }
